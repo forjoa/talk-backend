5ee088f9c6d60c5f7323957a8c825e63784f184b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-03 17:16:45 +0200

    init repo

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+const httpServer = createServer(app)
+
+const io = new Server(httpServer)
+
+io.on('connection', (socket) => {
+  console.log('a user is connected')
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room)
+    console.log(`User joined room ${room}`)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', ({ room, msg }) => {
+    io.to(room).emit('chat message', msg)
+  })
+})
+
+httpServer.listen(port, () => {
+  console.log(`> Server is running on http://localhost:${port}`)
+})
