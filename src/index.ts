1e5e8ed953b94ae13c287ed59d48d3ebb03d5cf0
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-03 17:21:23 +0200

    add types declaration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -11,10 +11,10 @@ const httpServer = createServer(app)
 
 const io = new Server(httpServer)
 
-io.on('connection', (socket) => {
+io.on('connection', (socket: Socket) => {
   console.log('a user is connected')
 
-  socket.on('joinRoom', (room) => {
+  socket.on('joinRoom', (room: string) => {
     socket.join(room)
     console.log(`User joined room ${room}`)
   })
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 
-  socket.on('chat message', ({ room, msg }) => {
+  socket.on('chat message', ({ room, msg }: { room: string; msg: string }) => {
     io.to(room).emit('chat message', msg)
   })
 })
