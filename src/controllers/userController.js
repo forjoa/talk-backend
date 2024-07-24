41dc2221c8148801d81997a89540b95455e841d4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-25 00:09:51 +0200

    build: user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,20 @@
+import { getAllUsers, updateUser } from '../services/userService';
+
+export const getUsers = async (req, res) => {
+    const { user_id } = req.params;
+    try {
+        const users = await getAllUsers(user_id);
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const updateUserProfile = async (req, res) => {
+    try {
+        const result = await updateUser(req.body);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
