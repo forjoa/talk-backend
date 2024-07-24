a9eb024dbe76981c5828abb1045f832be7ed1dda
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-25 00:12:44 +0200

    build: chat service

---
+++ src/services/chatService.js
@@ -0,0 +1,32 @@
+import { db } from '../config/database';
+
+export const getMyChats = async (id) => {
+    const { rows } = await db.execute({
+        sql: 'SELECT c.conversation_id, CASE WHEN u1.user_id = ? THEN u2.user_id ELSE u1.user_id END AS other_user_id, CASE WHEN u1.user_id = ? THEN u2.username ELSE u1.username END AS other_username, CASE WHEN u1.user_id = ? THEN u2.fullname ELSE u1.fullname END AS other_fullname FROM conversations c JOIN users u1 ON c.user1_id = u1.user_id JOIN users u2 ON c.user2_id = u2.user_id WHERE c.user1_id = ? OR c.user2_id = ?;',
+        args: [id, id, id, id, id],
+    });
+    return rows;
+};
+
+export const searchUser = async (username) => {
+    const { rows } = await db.execute({
+        sql: 'SELECT * FROM users WHERE LOWER(username) LIKE LOWER(?)',
+        args: [`%${username}%`],
+    });
+    return rows;
+};
+
+export const getConversation = async (conversation_id) => {
+    const { rows } = await db.execute({
+        sql: 'SELECT m.message_id, m.conversation_id, m.sender_id, u.username AS sender_username, u.fullname AS sender_fullname, m.content, m.timestamp FROM messages m JOIN users u ON m.sender_id = u.user_id WHERE m.conversation_id = ? ORDER BY m.timestamp ASC;',
+        args: [conversation_id],
+    });
+    return rows;
+};
+
+export const insertMessage = async ({ conversationId, senderId, content }) => {
+    await db.execute({
+        sql: 'INSERT INTO messages (conversation_id, sender_id, content) VALUES (?,?,?)',
+        args: [conversationId, senderId, content],
+    });
+};
