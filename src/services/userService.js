ff2b7584bf2b286d191ecbf2088e38aa1e85741e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-04 13:46:35 +0200

    fix: checking if username exists

--- src/services/userService.js
+++ src/services/userService.js
@@ -9,15 +9,8 @@ export const getAllUsers = async (user_id) => {
 };
 
 export const updateUser = async ({ user_id, username, fullname }) => {
-    const { rows } = await db.execute({
-        sql: 'SELECT * FROM users WHERE username = ?',
-        args: [username],
-    });
-
-    if (rows.length > 0) return { success: false, message: 'Username already exists' };
-
     const { rowsAffected } = await db.execute({
-        sql: 'UPDATE users SET username = ?, fullname = ? WHERE user_id = ?',
+        sql: 'UPDATE users SET fullname = ? WHERE user_id = ?',
         args: [username, fullname, user_id],
     });
 
